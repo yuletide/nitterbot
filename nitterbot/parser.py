91fcffb662f051b0619fa1e22554268ac7312114
Alex Yule <adyule@gmail.com>
2022-12-21 02:26:07 -0500

    add html parsing

---
+++ nitterbot/parser.py
@@ -0,0 +1,16 @@
+from html.parser import HTMLParser
+
+# https://stackoverflow.com/a/62180428/263926
+
+
+class HTMLFilter(HTMLParser):
+    text = ""
+
+    def handle_data(self, data):
+        self.text += data
+
+    @classmethod
+    def convert_html_to_text(cls, html: str) -> str:
+        f = cls()
+        f.feed(html)
+        return f.text.strip()
