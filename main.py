f55fa2124ee3b04720e059190c15584d53d37a2d
Alex Yule <adyule@gmail.com>
2022-12-21 22:44:47 -0500

    Asyc = true

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from nitterbot import bot
 def main():
     listener = notifylistener.NotifyListener()
     mastodon = bot.init()
-    mastodon.stream_user(listener)
+    # Mastodon.py currently does not support websocket based, multiplexed streams,
+    # but might in the future.
+    # https://mastodonpy.readthedocs.io/en/stable/10_streaming.html
+    mastodon.stream_user(listener, run_async=True)
 
 
 if __name__ == "__main__":
